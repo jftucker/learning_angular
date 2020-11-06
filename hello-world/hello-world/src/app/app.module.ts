f2c088517a6ef140799273cd9b72ad8153cc4cb5
john <johntucker0686@gmail.com>
2020-11-05 17:58:10 -0800

    angular fundamentals

--- hello-world/hello-world/src/app/app.module.ts
+++ hello-world/hello-world/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    CoursesComponent,
+    AuthorsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [CoursesService, AuthorsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
